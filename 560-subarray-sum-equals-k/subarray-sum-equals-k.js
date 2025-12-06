93b5e30d94360446560aede70fadf2e903da42ce
Tanisha Rai <75983583+tanisharai21@users.noreply.github.com>
2025-12-07 01:19:57 +0530

    Time: 19 ms (56.84%) | Memory: 64.8 MB (34.92%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function(nums, k) {
+    const prefixSum = new Map();
+    let result = 0;
+    let currSum = 0;
+    for(let num of nums){
+        currSum += num;
+        if(currSum === k){
+            result++;
+        }
+        if(prefixSum.has(currSum-k)){
+            result += prefixSum.get(currSum-k);
+        }
+        prefixSum.set(currSum,(prefixSum.get(currSum) || 0)+1);
+    }
+    return result;
+};
