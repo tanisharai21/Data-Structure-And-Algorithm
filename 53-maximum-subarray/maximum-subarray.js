ce2bc1f501fb048c9429502b6257f2d8bcf4d052
Tanisha Rai <75983583+tanisharai21@users.noreply.github.com>
2026-01-03 15:14:40 +0530

    Time: 2 ms (68.13%) | Memory: 63.9 MB (69.44%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let currSum = nums[0];
+    let maxSum = nums[0];
+
+    for(let i = 1;i<nums.length;i++){
+        currSum = Math.max(nums[i],currSum + nums[i]);
+        maxSum = Math.max(currSum, maxSum);
+    }
+    return maxSum;
+};
