e0976ec572a3e3a0ed2712f82d2929aa47ee7a58
Tanisha Rai <75983583+tanisharai21@users.noreply.github.com>
2026-02-02 20:30:41 +0530

    Time: 2 ms (92.94%) | Memory: 56.3 MB (88.70%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    let str = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+    let left = 0;
+    let right = str.length-1;
+    while(left<right){
+        if(str[left] !== str[right]){
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+};
