9ad1f870c702949ba90e542a83c0449272dab952
Tanisha Rai <75983583+tanisharai21@users.noreply.github.com>
2026-02-10 11:18:26 +0530

    Time: 0 ms (100.00%) | Memory: 55.6 MB (35.54%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -6,11 +6,10 @@
 var twoSum = function(nums, target) {
     let map = new Map();
     for(let i=0;i<nums.length;i++){
-          let compliment =  target- nums[i];
-          if(map.has(compliment)){
-            return [map.get(compliment),i];
+        let needed = target - nums[i];
+        if(map.has(needed)){
+            return [map.get(needed),i];
         }
         map.set(nums[i],i);
     }
-    return [];
 };
