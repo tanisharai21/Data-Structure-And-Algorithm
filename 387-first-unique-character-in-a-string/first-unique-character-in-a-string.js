479da7d8601d6eb7fb53025eb61f7dffb971e94b
Tanisha Rai <75983583+tanisharai21@users.noreply.github.com>
2026-02-11 10:48:07 +0530

    Time: 35 ms (45.13%) | Memory: 59.6 MB (71.28%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.js
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.js
@@ -4,12 +4,12 @@
  */
 var firstUniqChar = function(s) {
     let map = new Map();
-    //find frequency of each element
+    //find the frequency of each element
     for(let ch of s){
-        map.set(ch, (map.get(ch) || 0) + 1);
+        map.set(ch, (map.get(ch) || 0)+1);
     }
-    //first non repeating character
-    for(let i =0;i<s.length;i++){
+    //find first non repeating 
+    for(let i = 0;i<s.length;i++){ //we need to return the index so we are using i to traverse not ch
         if(map.get(s[i]) === 1){
             return i;
         }
