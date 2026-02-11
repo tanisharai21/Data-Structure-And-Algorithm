fc6ee662ecd76fe314f5bec44626201b7540bf84
Tanisha Rai <75983583+tanisharai21@users.noreply.github.com>
2026-02-11 11:03:54 +0530

    Time: 1 ms (49.36%) | Memory: 59.3 MB (5.16%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -3,10 +3,10 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    if(nums.length===0) return 0;
-    let i=0;
+    if(nums.length === 0) return 0;
+    let i = 0;
     for(let j=1;j<nums.length;j++){
-        if(nums[j] !== nums[i]){
+        if(nums[i] !== nums[j]){
             i++;
             nums[i] = nums[j];
         }
