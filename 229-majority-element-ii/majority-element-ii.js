c2ca2e1a0d27900b4ef12d6bf2919ed99ea1cae3
Tanisha Rai <75983583+tanisharai21@users.noreply.github.com>
2026-02-12 11:34:25 +0530

    Time: 2 ms (93.63%) | Memory: 57.6 MB (74.86%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var majorityElement = function(nums) {
+    let candidate1 = null, candidate2 = null;
+    let count1 = 0, count2 = 0;
+    //find potential element
+    for(let num of nums){
+        if(num === candidate1){
+            count1++;
+        }
+        else if(num === candidate2){
+            count2++;
+        }
+        else if(count1 === 0){
+            candidate1 = num;
+            count1 = 1;
+        }
+        else if(count2 === 0){
+            candidate2 = num;
+            count2 = 1;
+        }
+        else{
+            count1--;
+            count2--;
+        }
+    }
+
+    //varify count
+    let result  = [];
+    count1 = 0;
+    count2 = 0;
+    for(let num of nums){
+        if(num === candidate1) count1++;
+        else if(num === candidate2) count2++;
+    }
+    if(count1 > Math.floor(nums.length/3)){
+        result.push(candidate1);
+    }
+    if(count2 > Math.floor(nums.length/3)){
+        result.push(candidate2);
+    }
+    return result;
+};
