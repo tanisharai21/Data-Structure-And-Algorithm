6f8544e524874aa81039719ecff61f83a3a74cd9
Tanisha Rai <75983583+tanisharai21@users.noreply.github.com>
2026-03-25 14:59:23 +0530

    Time: 10 ms (42.47%) | Memory: 57.2 MB (52.05%) - LeetSync

--- 2186-count-vowel-substrings-of-a-string/count-vowel-substrings-of-a-string.js
+++ 2186-count-vowel-substrings-of-a-string/count-vowel-substrings-of-a-string.js
@@ -3,19 +3,20 @@
  * @return {number}
  */
 var countVowelSubstrings = function(word) {
-    let count = 0;
-    const vowel = new Set(['a','e','i','o','u']);
+    let count = 0; //to store the number of valid substrings
+    let vowel = new Set(['a','e','i','o','u']) //helps to check the vowel
+    //this loop picks the starting index 
     for(let i=0;i<word.length;i++){
-        let seen = new Set();
+        let seen = new Set(); //keeps the track of vowels found in current substring
+        //this loops is for substring
         for(let j=i;j<word.length;j++){
-        //check consonents
-        if(!vowel.has(word[j])){
-            break;
-        }
-        seen.add(word[j]);
-        if(seen.size===5){
-            count++;
-        }
+            if(!vowel.has(word[j])){ //if the current character is not vowel then break
+                break;
+            }
+            seen.add(word[j]); //add current vowel to the set
+            if(seen.size === 5){
+                count++;
+            }
         }
     }
     return count;
