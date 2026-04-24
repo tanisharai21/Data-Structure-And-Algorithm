4462fabde13c2b1a5686e8917f69bf17f535c400
Tanisha Rai <75983583+tanisharai21@users.noreply.github.com>
2026-04-24 16:27:12 +0530

    Time: 0 ms (100.00%) | Memory: 53.9 MB (36.04%) - LeetSync

--- 27-remove-element/remove-element.js
+++ 27-remove-element/remove-element.js
@@ -4,10 +4,9 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-    //we will use two pointer to remove the specific elements
-    let i=0;
-    for(let j=0;j<nums.length;j++){
-        if(nums[j] !== val){
+    let i = 0;
+    for(let j = 0;j < nums.length; j++){
+        if(nums[j] != val){
             nums[i] = nums[j];
             i++;
         }
