1a4476fed44f59c03ab000dec7625cc1450a109f
Tanisha Rai <75983583+tanisharai21@users.noreply.github.com>
2026-04-24 16:22:56 +0530

    Time: 1 ms (47.67%) | Memory: 58.8 MB (10.35%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -3,10 +3,11 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    if(nums.length === 0)return 0;
-    let i =0;
+    //base case
+    if(nums.length===0) return 0;
+    let i=0;
     for(let j=1;j<nums.length;j++){
-        if(nums[i] !== nums[j]){
+        if(nums[i]!==nums[j]){
             i++;
             nums[i] = nums[j];
         }
